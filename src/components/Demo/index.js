94ac170b68477313742ba440adccbb3f205e30ea
Floriana Domianello <floriana.domianello@gmail.com>
2022-05-06 12:02:06 +0200

    prova

---
+++ src/components/Demo/index.js
@@ -0,0 +1,54 @@
+import * as React from 'react';
+import Paper from '@mui/material/Paper';
+import { ViewState } from '@devexpress/dx-react-scheduler';
+import {
+  Scheduler,
+  DayView,
+  WeekView,
+  Appointments,
+  Toolbar,
+  ViewSwitcher,
+} from '@devexpress/dx-react-scheduler-material-ui';
+
+import { appointments } from '../../../demo-data/month-appointments';
+
+export default class Demo extends React.PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: appointments,
+    };
+  }
+
+  render() {
+    const { data } = this.state;
+
+    return (
+      <Paper>
+        <Scheduler
+          data={data}
+          height={660}
+        >
+          <ViewState
+            defaultCurrentDate="2018-07-25"
+            defaultCurrentViewName="Week"
+          />
+
+          <DayView
+            startDayHour={9}
+            endDayHour={18}
+          />
+          <WeekView
+            startDayHour={10}
+            endDayHour={19}
+          />
+
+          <Toolbar />
+          <ViewSwitcher />
+          <Appointments />
+        </Scheduler>
+      </Paper>
+    );
+  }
+}
